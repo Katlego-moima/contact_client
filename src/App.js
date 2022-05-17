c5719995f80e7134f6bc6fdc83c1b1793cd44f9f
katlego <moimakatlego@outlook.com>
2022-05-17 12:37:37 +0200

    Home page done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Home from './pages/Home';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <h1>hello from react</h1>
+      <ToastContainer position="top-ceter" />
+      
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+        </Routes>
+    
     </div>
+    </Router>
   );
 }
 
